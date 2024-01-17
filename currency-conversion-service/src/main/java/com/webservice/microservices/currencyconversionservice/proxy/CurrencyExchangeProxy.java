9bd20a5a12a17bab136272b95f97d6d7b008d794
Naveen <neeratinaveen675@gmail.com>
2024-01-18 02:09:59 +0530

    added eureka server

---
+++ currency-conversion-service/src/main/java/com/webservice/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,14 @@
+package com.webservice.microservices.currencyconversionservice.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.webservice.microservices.currencyconversionservice.exchanges.CurrencyConversion;
+
+
+@FeignClient(name="currency-exchange", url="localhost:8000")
+public interface CurrencyExchangeProxy{
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(@PathVariable(name = "from",required = true) String from,@PathVariable(name = "to",required = true)  String to);
+}
