670add19d2c753540739620b11060398e4a68ab9
Naveen <neeratinaveen675@gmail.com>
2024-01-19 01:05:32 +0530

    Added eureka server,api gateway and resilience4j

--- currency-conversion-service/src/main/java/com/webservice/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/webservice/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.webservice.microservices.currencyconversionservice.exchanges.CurrencyConversion;
 
 
-@FeignClient(name="currency-exchange", url="localhost:8000")
+@FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy{
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversion retrieveExchangeValue(@PathVariable(name = "from",required = true) String from,@PathVariable(name = "to",required = true)  String to);
